a3415cca43262af11fd03904d416adca40ff098e
paulooosf <phpessoal2021@gmail.com>
2024-04-04 18:23:11 -0300

    Corrigindo entidades

--- src/br/com/paulo/projeto/entities/Denuncia.java
+++ src/br/com/paulo/projeto/entities/Denuncia.java
@@ -1,5 +1,65 @@
 package br.com.paulo.projeto.entities;
 
-public class Denuncia {
+import java.time.LocalDate;
 
+public class Denuncia {
+	
+	private int id;
+	private LocalDate data;
+	private String detalhamento;
+	private boolean anonimo;
+	private int fkUsu;
+	
+	public Denuncia() {
+		super();
+	}
+
+	public Denuncia(int id, LocalDate data, String detalhamento, boolean anonimo, int fkUsu) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.detalhamento = detalhamento;
+		this.anonimo = anonimo;
+		this.fkUsu = fkUsu;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public String getDetalhamento() {
+		return detalhamento;
+	}
+
+	public void setDetalhamento(String detalhamento) {
+		this.detalhamento = detalhamento;
+	}
+
+	public boolean isAnonimo() {
+		return anonimo;
+	}
+
+	public void setAnonimo(boolean anonimo) {
+		this.anonimo = anonimo;
+	}
+
+	public int getFkUsu() {
+		return fkUsu;
+	}
+
+	public void setFkUsu(int fkUsu) {
+		this.fkUsu = fkUsu;
+	}
 }
--- src/br/com/paulo/projeto/entities/Usuario.java
+++ src/br/com/paulo/projeto/entities/Usuario.java
@@ -1,5 +1,74 @@
 package br.com.paulo.projeto.entities;
 
 public class Usuario {
+	
+	private int id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String cpf;
+	private String genero;
+	
+	public Usuario() {
+		super();
+	}
 
+	public Usuario(int id, String nome, String email, String telefone, String cpf, String genero) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		this.genero = genero;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
 }

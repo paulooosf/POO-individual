ed024871fcc09999f2ba193feb41dec5b8ed47e6
paulooosf <phpessoal2021@gmail.com>
2024-04-15 00:24:32 -0300

    feat: Tratamento de erros

--- src/br/com/paulo/projeto/services/MenuService.java
+++ src/br/com/paulo/projeto/services/MenuService.java
@@ -1,6 +1,7 @@
 package br.com.paulo.projeto.services;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,34 +18,38 @@ public class MenuService {
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void menu() throws IOException {
-		
-		Util.customizer();
-		logger.log(Level.INFO, () -> ""
-					+ "Menu interativo:\n"
-					+ "[1] Relatório na Tela\n"
-					+ "[2] Relatório Impresso\n"
-					+ "[3] Sair\n"
-					+ "\nDigite uma opção: ");
-		
-		int opcao = sc.nextInt();
-		
-		switch(opcao) {
-		
-		case 1:
-			SubMenuService.subMenuTela();
-			break;
-		case 2:
-			SubMenuService.subMenuImpresso();
-			break;
-		case 3:
-			Util.customizer();
-			logger.log(Level.INFO, "Até a próxima!");
-			break;
-		default:
-			Util.customizer();
-			logger.log(Level.WARNING, "Opção inválida!");
-			menu();
-			break;
+		try {
+			Util.customizer();			                                 
+			logger.log(Level.INFO, () -> ""
+						+ "Menu interativo:\n"
+						+ "[1] Relatório na Tela\n"
+						+ "[2] Relatório Impresso\n"
+						+ "[3] Sair\n"
+						+ "\nDigite uma opção: ");
+			
+			int opcao = sc.nextInt();
+			
+			switch(opcao) {
+			
+			case 1:
+				SubMenuService.subMenuTela();
+				break;
+			case 2:
+				SubMenuService.subMenuImpresso();
+				break;
+			case 3:
+				Util.customizer();
+				logger.log(Level.INFO, "Até a próxima!");
+				break;
+			default:
+				Util.customizer();
+				logger.log(Level.WARNING, "Opção inválida!");
+				menu();
+				break;
+			}
+		}
+		catch (InputMismatchException e) {
+			logger.log(Level.INFO, "Digite um número inteiro!");
 		}
 	}
 }
